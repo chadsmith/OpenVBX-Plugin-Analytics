b3e3e59b9aaa8b2f71db9484fde5ef37f35113dc
Chad Smith <chad@nospam.me>
2011-02-16 21:56:57 -0600

    Update for API version 2010-04-01

--- applets/track/twiml.php
+++ applets/track/twiml.php
@@ -4,27 +4,18 @@ $url = AppletInstance::getValue('url');
 $title = AppletInstance::getValue('title');
 $next = AppletInstance::getDropZoneUrl('next');
 
-if(AppletInstance::getFlowType() == 'voice'){
-	$caller = normalize_phone_to_E164($_REQUEST['Caller']);
-	$called = normalize_phone_to_E164($_REQUEST['Called']);
-	$url = str_replace(array('%caller%', '%number%'), array($caller, $called), $url);
-	$title = str_replace(array('%caller%', '%number%'), array($caller, $called), $title);
-}
-else{
-	$from = normalize_phone_to_E164($_REQUEST['From']);
-	$to = normalize_phone_to_E164($_REQUEST['To']);
-	$url = str_replace(array('%caller%', '%number%'), array($from, $to), $url);
-	$title = str_replace(array('%caller%','%number%'), array($from, $to), $title);
-}
+$from = normalize_phone_to_E164($_REQUEST['From']);
+$to = normalize_phone_to_E164($_REQUEST['To']);
+$url = str_replace(array('%caller%', '%number%'), array($from, $to), $url);
+$title = str_replace(array('%caller%','%number%'), array($from, $to), $title);
 
 include_once('Galvanize.php');
 @$GA = new Galvanize($account);
 @$GA->trackPageView($url, $title);
 
 $response = new Response();
+
 if(!empty($next))
-{
     $response->addRedirect($next);
-}
 
 $response->Respond();
