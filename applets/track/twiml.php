2cfbdf7d50acd71b0c65a78e4538328dac59db4c
Chad Smith <chad@nospam.me>
2011-05-20 01:19:17 -0500

    Fix PHP notices

--- applets/track/twiml.php
+++ applets/track/twiml.php
@@ -4,14 +4,16 @@ $url = AppletInstance::getValue('url');
 $title = AppletInstance::getValue('title');
 $next = AppletInstance::getDropZoneUrl('next');
 
-$from = normalize_phone_to_E164($_REQUEST['From']);
-$to = normalize_phone_to_E164($_REQUEST['To']);
-$url = str_replace(array('%caller%', '%number%'), array($from, $to), $url);
-$title = str_replace(array('%caller%','%number%'), array($from, $to), $title);
+if(!empty($_REQUEST['From'])) {
+    $from = normalize_phone_to_E164($_REQUEST['From']);
+    $to = normalize_phone_to_E164($_REQUEST['To']);
+    $url = str_replace(array('%caller%', '%number%'), array($from, $to), $url);
+    $title = str_replace(array('%caller%','%number%'), array($from, $to), $title);
 
-include_once('Galvanize.php');
-@$GA = new Galvanize($account);
-@$GA->trackPageView($url, $title);
+    include_once('Galvanize.php');
+    @$GA = new Galvanize($account);
+    @$GA->trackPageView($url, $title);
+}
 
 $response = new Response();
 
