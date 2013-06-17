e734f9d35d5420b88446eebe68920a9d3af7c295
Chad Smith <chad@nospam.me>
2013-06-17 10:02:32 -0500

    Use Measurement Protocol instead of Galvanize.php

--- applets/track/twiml.php
+++ applets/track/twiml.php
@@ -9,10 +9,20 @@ if(!empty($_REQUEST['From'])) {
     $to = normalize_phone_to_E164($_REQUEST['To']);
     $url = str_replace(array('%caller%', '%number%'), array($from, $to), $url);
     $title = str_replace(array('%caller%','%number%'), array($from, $to), $title);
-
-    include_once('Galvanize.php');
-    @$GA = new Galvanize($account);
-    @$GA->trackPageView($url, $title);
+    $ch = curl_init('http://www.google-analytics.com/collect');
+    $fields = array(
+      'v' => 1,
+      'tid' => $account,
+      'cid' => substr($from, 1),
+      't' => 'pageview',
+      'dp' => $url,
+      'dt' => $title
+    );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, count($fields));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
+    curl_exec($ch);
+    curl_close($ch);
 }
 
 $response = new TwimlResponse;
